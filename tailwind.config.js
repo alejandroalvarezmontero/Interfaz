a7842e08c704a6cead423e8dbab27b836805e304
alejandroalvarezmontero <95092635+alejandroalvarezmontero@users.noreply.github.com>
2023-01-19 17:47:42 +0100

    creacion y edicion de ficheros

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports= {
+    theme:{
+        extend: {
+            backgroundImage:{
+                'hero-pattern': "url('/img/hero-pattern.svg')",
+                'footer-texture': "url('/img/footer-texture.png')",
+                animation: {
+                    'spin-slow': 'spin 3s linear infinite',
+                  }
+              
+            }
+        }
+    }
+}
